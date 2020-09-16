041b2490e56cf3a6f1200259dcc19822206c697e
oleg <mupastir@gmail.com>
2020-09-16 09:35:45 +0300

    Update import script

--- import_backup.sh
+++ import_backup.sh
@@ -3,6 +3,11 @@
 # copy themes to local themes
 cp -urv .themes ~/
 
+# copu icons to local icons
+
+cp -urv .icons ~/
+
 # copy xfce settings
 
-cp -urv .config/xfce ~/.config
+cp -urv .config/xfce4 ~/.config
+
